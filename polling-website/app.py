13558895d7d62bba5ceb9193212d7453b915777e
Aphanvahrius <apanvarius@gmail.com>
2024-06-13 03:05:35 +0200

    modified:   polling-website/app.py

--- polling-website/app.py
+++ polling-website/app.py
@@ -6,42 +6,57 @@ from psycopg2.extras import RealDictCursor
 from config import Config
 from database import init_db, insert_poll_response
 from azure_storage import AzureBlobStorage
+from forms import PollForm  # Import the PollForm class
 
 app = Flask(__name__, static_url_path='/static')
 app.config.from_object(Config)
+app.config['SECRET_KEY'] = Config.SECRET_KEY
 
 azure_blob_storage = AzureBlobStorage(Config.AZURE_STORAGE_CONNECTION_STRING, Config.AZURE_STORAGE_CONTAINER_NAME)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    form = PollForm()
+    return render_template('index.html', form=form)
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    age = request.form['age']
-    gender = request.form['gender']
-    education = request.form['education']
-    support = request.form['support']
-    building_type = request.form.getlist('building_type')
-    environment_importance = request.form['environment_importance']
-    opinion = request.form.get('opinion', '')
-        
-    file_url = None
-    if 'attachment' in request.files:
-        file = request.files['attachment']
-        if file and file.filename != '':
-            file_url = azure_blob_storage.upload_file(file)
+    form = PollForm()
+    if form.validate_on_submit():
+        age = form.age.data
+        gender = form.gender.data
+        education = form.education.data
+        support = form.support.data
+        building_type = []
+        if form.building_type_hospital.data:
+            building_type.append('hospital')
+        if form.building_type_school.data:
+            building_type.append('school')
+        if form.building_type_mall.data:
+            building_type.append('mall')
+        if form.building_type_park.data:
+            building_type.append('park')
+        if form.building_type_office.data:
+            building_type.append('office')
+        environment_importance = form.environment_importance.data
+        opinion = form.opinion.data
 
+        file_url = None
+        if 'attachment' in request.files:
+            file = request.files['attachment']
+            if file and file.filename != '':
+                file_url = azure_blob_storage.upload_file(file)
 
-    # Insert data into the database
-    try:
-        insert_poll_response(age, gender, education, support, building_type, environment_importance, opinion, file_url)
-        flash('Your response has been submitted successfully!', 'success')
-    except Exception as e:
-        flash(str(e), 'danger')
+        try:
+            insert_poll_response(age, gender, education, support, building_type, environment_importance, opinion, file_url)
+            flash('Your response has been submitted successfully!', 'success')
+        except Exception as e:
+            flash(str(e), 'danger')
 
-    return redirect('/')
+        return redirect('/')
+    else:
+        flash('There was an error with your submission. Please check your inputs and try again.', 'danger')
+        return render_template('index.html', form=form)
 
 if __name__ == '__main__':
-    init_db()
     app.run(debug=True)
